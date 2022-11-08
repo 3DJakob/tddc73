40d5d45733859db23b2673e83965d3f275b429d3
Jakob Unnebäck <jakob@folkdatorn.se>
2022-11-08 11:58:25 +0100

    Buttons and input

--- lab1/flutter_app/lib/homepage.dart
+++ lab1/flutter_app/lib/homepage.dart
@@ -16,14 +16,64 @@ class MyHomePage extends StatelessWidget {
           children: <Widget>[
             const Image(
                 image: AssetImage('images/sample.png'), width: 80, height: 80),
-            TextButton(
-              style: ButtonStyle(
-                foregroundColor: MaterialStateProperty.all<Color>(Colors.blue),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextButton(
+                  style: ButtonStyle(
+                    foregroundColor:
+                        MaterialStateProperty.all<Color>(Colors.blue),
+                  ),
+                  onPressed: () {
+                    print('You clicked the button!');
+                  },
+                  child: const Text('TextButton 1'),
+                ),
+                TextButton(
+                  style: ButtonStyle(
+                    foregroundColor:
+                        MaterialStateProperty.all<Color>(Colors.blue),
+                  ),
+                  onPressed: () {
+                    print('You clicked the button!');
+                  },
+                  child: const Text('TextButton 2'),
+                )
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextButton(
+                  style: ButtonStyle(
+                    foregroundColor:
+                        MaterialStateProperty.all<Color>(Colors.blue),
+                  ),
+                  onPressed: () {
+                    print('You clicked the button!');
+                  },
+                  child: const Text('TextButton 1'),
+                ),
+                TextButton(
+                  style: ButtonStyle(
+                    foregroundColor:
+                        MaterialStateProperty.all<Color>(Colors.blue),
+                  ),
+                  onPressed: () {
+                    print('You clicked the button!');
+                  },
+                  child: const Text('TextButton 2'),
+                )
+              ],
+            ),
+            Container(
+              margin: const EdgeInsets.only(left: 20.0, right: 20.0),
+              child: const TextField(
+                decoration: const InputDecoration(
+                  border: const OutlineInputBorder(),
+                  labelText: 'Enter your name',
+                ),
               ),
-              onPressed: () {
-                print('You clicked the button!');
-              },
-              child: Text('TextButton'),
             )
           ],
         ),
