254f689d4822f10f42f32f25b8cef953ade5ae78
Jakob Unnebäck <jakob@folkdatorn.se>
2022-12-02 15:13:27 +0100

    Refactor browse page

--- lab3/App.tsx
+++ lab3/App.tsx
@@ -1,67 +1,15 @@
 import { StatusBar } from 'expo-status-bar'
 import { ApolloClient, ApolloProvider } from '@apollo/client'
-import useTrendingRepos from './src/hooks/useTrendingRepos'
-import RepoThumbnail from './src/components/RepoThumbnail'
-import styled from 'styled-components/native'
-import { Picker } from '@react-native-picker/picker'
-import { useState } from 'react'
 import { clientConfig } from './API'
-import { ActivityIndicator } from 'react-native'
+import Browse from './src/pages/Browse'
 
-const Container = styled.ScrollView`
-  background-color: #262626;
-`
-
-const Center = styled.View`
-  flex: 1;
-  align-items: center;
-  justify-content: center;
-`
-
-export const client = new ApolloClient(clientConfig)
-
-const topics = [
-  'python',
-  'javascript',
-  'java',
-  'ruby',
-  'go',
-  'c',
-  'c++',
-  'c#',
-  'php',
-  'swift',
-  'kotlin',
-  'rust',
-  'scala'
-]
+const client = new ApolloClient(clientConfig)
 
 const App: React.FC = () => {
-  const [selectedLanguage, setSelectedLanguage] = useState(topics[0])
-  const [repos, loading] = useTrendingRepos(selectedLanguage)
-  console.log(repos)
   return (
     <ApolloProvider client={client}>
-      <Picker
-        style={{ backgroundColor: '#111', color: '#fff' }}
-        itemStyle={{ color: '#fff' }}
-        selectedValue={selectedLanguage}
-        onValueChange={(itemValue, itemIndex) =>
-          setSelectedLanguage(itemValue)}
-      >
-        {
-          topics.map((topic) => (
-            <Picker.Item key={topic} label={topic} value={topic} />
-          ))
-        }
-      </Picker>
-      <Container>
-        {!loading && repos.map((repo) => (
-          <RepoThumbnail key={repo.description} repoInfo={repo} />
-        ))}
-        {loading && <Center><ActivityIndicator /></Center>}
-        <StatusBar style='auto' />
-      </Container>
+      <Browse />
+      <StatusBar style='auto' />
     </ApolloProvider>
   )
 }
---
+++ lab3/src/pages/Browse.tsx
@@ -0,0 +1,62 @@
+import styled from 'styled-components/native'
+import { Picker } from '@react-native-picker/picker'
+import { useState } from 'react'
+import { ActivityIndicator } from 'react-native'
+import useTrendingRepos from '../hooks/useTrendingRepos'
+import RepoThumbnail from '../components/RepoThumbnail'
+
+const Container = styled.ScrollView`
+  background-color: #262626;
+`
+
+const Center = styled.View`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+`
+
+const topics = [
+  'python',
+  'javascript',
+  'java',
+  'ruby',
+  'go',
+  'c',
+  'c++',
+  'c#',
+  'php',
+  'swift',
+  'kotlin',
+  'rust',
+  'scala'
+]
+
+const Browse: React.FC = () => {
+  const [selectedLanguage, setSelectedLanguage] = useState(topics[0])
+  const [repos, loading] = useTrendingRepos(selectedLanguage)
+  return (
+    <>
+      <Picker
+        style={{ backgroundColor: '#111', color: '#fff' }}
+        itemStyle={{ color: '#fff' }}
+        selectedValue={selectedLanguage}
+        onValueChange={(itemValue, itemIndex) =>
+          setSelectedLanguage(itemValue)}
+      >
+        {
+          topics.map((topic) => (
+            <Picker.Item key={topic} label={topic} value={topic} />
+          ))
+        }
+      </Picker>
+      <Container>
+        {!loading && repos.map((repo) => (
+          <RepoThumbnail key={repo.description} repoInfo={repo} />
+        ))}
+        {loading && <Center><ActivityIndicator /></Center>}
+      </Container>
+    </>
+  )
+}
+
+export default Browse
